ca9b2e7a61bca15e0609b87bd9086b8a299bcdd0
Zoimback <rgalexmv@gmail.com>
2025-01-31 18:16:16 +0100

    Upload SQL file

--- database.sql
+++ database.sql
@@ -1,10 +1,38 @@
-CREATE DATABASE Audit;
+CREATE DATABASE jenkins;
+
+CREATE TABLE audit (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        Aplication VARCHAR(50) NOT NULL,
+        Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        Repository VARCHAR(150) NOT NULL,
+        End ENUM('Good', 'Bad') NOT NULL,
+        Branch VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE despliegues (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Aplicacion VARCHAR(50) NOT NULL,
-    Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Repositorio VARCHAR(150) NOT NULL,
-    Finalizacion ENUM('Bien', 'Mal') NOT NULL,
-    Rama VARCHAR(100) NOT NULL
-);
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        Aplication VARCHAR(50) NOT NULL,
+        Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        Repository VARCHAR(150) NOT NULL,
+        Ends ENUM('Bien', 'Mal') NOT NULL,
+        Branch VARCHAR(100) NOT NULL,
+        Path VARCHAR(100) NOT NULL,
+        ServerName VARCHAR(100) NOT NULL
+    );
+
+
+CREATE DATABASE SensorData;
+
+    CREATE TABLE data (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        Temperatura Double NOT NULL,
+        Posicion VARCHAR(50) NOT NULL,
+        Nombre VARCHAR(50) NOT NULL,
+        Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+    );
+
+    CREATE TABLE sensor (
+        Name VARCHAR(50) PRIMARY KEY,
+        Batery Double NOT NULL,
+        Estado ENUM('Activado', 'Desactivado') NOT NULL
+    );
